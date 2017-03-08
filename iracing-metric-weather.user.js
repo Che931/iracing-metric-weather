4b8288faf8e24a4b067f64bf9ebc9e53048284b9
Fran Romero <Che931@users.noreply.github.com>
2017-03-08 19:55:05 +0100

    Added userscript
    First functional version of this script

---
+++ iracing-metric-weather.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name            iRacing Metric Weather
+// @version         0.0.1
+// @description     This script converts iracing event result weather from imperial to metric units (wind speed and temperature)
+// @author          Francisco J Romero
+// @licence         MIT
+// @namespace       https://github.com/Che931
+// @match           *://members.iracing.com/membersite/member/EventResult.*
+// @run-at          document-end
+// @grant           none
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    function ConvertWeather() {
+        var weather_conditions = document.getElementsByClassName("event_datawidth15 Conditions-TD")[0];
+        var stringArray = weather_conditions.innerText.split(',');
+
+        var dynamic = /\D{7}/.exec(stringArray[0]);
+        var isDynamic = dynamic !== null;
+
+        var fahTemp = /\d{2}/.exec(stringArray[0]);
+        var celsius = Math.trunc(FahrenheitToCelsius(parseInt(fahTemp)));
+
+        var windDir = /\s[NSEW]+/.exec(stringArray[1].trim());
+        var mph =  /\d+/.exec(stringArray[1]);
+        var kph = Math.trunc(MphToKph(parseInt(mph)));
+
+        var p = document.createElement('p');
+        var text = ''+ celsius +'ºC, Wind ' + windDir + ' @ ' + kph + ' KPH,';
+
+        if (isDynamic) {
+            text = "<b>Dynamic: </b> " + text;
+        }
+
+        p.innerHTML = text + "<br>" + stringArray[2] + "<br>" + stringArray[3];
+
+        weather_conditions.innerText = "";
+        weather_conditions.append(p);
+    }
+
+    function FahrenheitToCelsius(value)
+    {
+        return (value - 32) / 1.8;
+    }
+
+    function MphToKph(value)
+    {
+        return value * 1.60934;
+    }
+
+    ConvertWeather();
+
+})();
