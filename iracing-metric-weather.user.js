1dee4af07a26f9bfd4d5f55ff92daeaa77318d90
Che931 <fj.romero93@gmail.com>
2018-10-16 02:18:35 +0200

    Update iracing-metric-weather.user.js
    V0.0.2
    
    - Added a small code to check if the script has to run. In rare ocasions, hosted results use metric units so scripts wouldn't work.

--- iracing-metric-weather.user.js
+++ iracing-metric-weather.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            iRacing Metric Weather
-// @version         0.0.1
+// @version         0.0.2
 // @description     This script converts iracing event result weather from imperial to metric units (wind speed and temperature)
 // @author          Francisco J Romero
 // @licence         MIT
@@ -12,22 +12,27 @@
 // @grant           none
 // ==/UserScript==
 
-(() => {
+(function() {
     'use strict';
 
     function ConvertWeather() {
         var weather_conditions = document.getElementsByClassName("event_datawidth15 Conditions-TD")[0];
         var stringArray = weather_conditions.innerText.split(',');
 
+        if (!/MPH/.test(stringArray[1])) { //Some hosted results contain metric units.
+            console.log("Metric units detected");
+            return;
+        }
+
         var dynamic = /\D{7}/.exec(stringArray[0]);
         var isDynamic = dynamic !== null;
 
         var fahTemp = /\d{2}/.exec(stringArray[0]);
-        var celsius = Math.trunc(FahrenheitToCelsius(parseInt(fahTemp)));
+        var celsius = FahrenheitToCelsius(parseInt(fahTemp));
 
         var windDir = /\s[NSEW]+/.exec(stringArray[1].trim());
         var mph =  /\d+/.exec(stringArray[1]);
-        var kph = Math.trunc(MphToKph(parseInt(mph)));
+        var kph = MphToKph(parseInt(mph));
 
         var p = document.createElement('p');
         var text = ''+ celsius +'ºC, Wind ' + windDir + ' @ ' + kph + ' KPH,';
@@ -44,12 +49,12 @@
 
     function FahrenheitToCelsius(value)
     {
-        return (value - 32) / 1.8;
+        return Math.trunc((value - 32) / 1.8);
     }
 
     function MphToKph(value)
     {
-        return value * 1.60934;
+        return Math.trunc(value * 1.60934);
     }
 
     ConvertWeather();
